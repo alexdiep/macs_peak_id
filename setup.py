17d203323b2f280b532da9bd118d08ba1cdf733a
Alex DIep <alexdiep2501@gmail.com>
2016-09-04 17:38:16 -0400

    added details to setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ setup(
       name='macs_peak_id',
       version='0.1',
       description='Small script to relate target and gene peaks.',
+      long_description='README.md',
       url='http://github.com/alexdiep/macs_peak_id',
       author='Alex Diep',
       author_email='alexdiep2501@gmail.com',
       license='MIT',
+      packages=['macs_peak_id'],
+      install_requires=['pandas'],
       entry_points={
             'console_scripts': ['macs_peak_id=macs_peak_id.cli:main']
       },
+      include_package_data=True,
       zip_safe=False
 )
