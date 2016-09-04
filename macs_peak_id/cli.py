6cb265696d71f0f1b19a9384052460b646239a23
Alex DIep <alexdiep2501@gmail.com>
2016-09-04 16:20:25 -0400

    added function to get data from filter file

--- macs_peak_id/cli.py
+++ macs_peak_id/cli.py
@@ -1,14 +1,28 @@
 from argparse import ArgumentParser
+import os
+
+def filter(file):
+    with open('targetnoN.fa', 'r') as target:
+         target_series = pd.read_table(target, delimiter='/n', squeeze=True, header=None)
+         # Every fourth row of data is the revelant one
+         return target_series[::4]
+
+
 
 def arguments():
     parser = ArgumentParser()
 
     parser.add_argument('file', help='Test description')
 
+    parser.add_argument('filter', help='Test description')
+
     return parser.parse_args()
 
 def main():
-    print(arguments())
+    args = arguments()
+    
+    filterlist = filter(args.filter)
+
 
 if __name__ == "__main__":
     main()
