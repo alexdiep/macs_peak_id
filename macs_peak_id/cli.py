71db8ea84ae75e46e0f7d8bd5ec340ae176077bb
Alex DIep <alexdiep2501@gmail.com>
2016-09-04 16:21:59 -0400

    used argument for filter

--- macs_peak_id/cli.py
+++ macs_peak_id/cli.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 import os
 
 def filter(file):
-    with open('targetnoN.fa', 'r') as target:
+    with open(file, 'r') as target:
          target_series = pd.read_table(target, delimiter='/n', squeeze=True, header=None)
          # Every fourth row of data is the revelant one
          return target_series[::4]
