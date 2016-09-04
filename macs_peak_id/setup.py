e29be54bfa41135083c37d01312795aafaabd79b
Alex DIep <alexdiep2501@gmail.com>
2016-09-03 21:25:07 -0400

    reorganized script

---
+++ macs_peak_id/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mac_peak_id',
+      version='0.1',
+      description='Small script to relate target and gene peaks.',
+      url='http://github.com/alexdiep/macs_peak_id',
+      author='Alex Diep',
+      author_email='alexdiep2501@gmail.com',
+      license='MIT',
+      packages=['macs_peak_id'],
+      include_package_data=True,
+      zip_safe=False)
