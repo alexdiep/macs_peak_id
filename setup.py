39a91e67f4f1142f42b43200e5c48518189468dc
Alex DIep <alexdiep2501@gmail.com>
2016-09-17 17:27:45 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
       name='macs_peak_id',
       version='0.1',
-      description='Small script to relate target and gene peaks.',
+      description='Mac_peak_id analyzes *.bed and target.fa files from HOMER.',
       long_description='README.md',
       url='http://github.com/alexdiep/macs_peak_id',
       author='Alex Diep',
       author_email='alexdiep2501@gmail.com',
       license='MIT',
-      packages=['macs_peak_id'],
-      install_requires=['pandas'],
+      packages=find_packages(),
+      install_requires=['pandas', 'biopython'],
       entry_points={
-            'console_scripts': ['macs_peak_id=macs_peak_id.cli:main']
+            'console_scripts': 
+                  ['macs_peak_id = macs_peak_id.main:main']
       },
       classifiers=[
         'Development Status :: 3 - Alpha',
-
-        # Pick your license as you wish (should match "license" above)
+        'Environment :: Console',
         'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
 ],
