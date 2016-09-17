6464bb144ec295a5663a0b987241f7829725bfd9
Alex DIep <alexdiep2501@gmail.com>
2016-09-17 17:35:30 -0400

    Added main.py woops

---
+++ macs_peak_id/main.py
@@ -0,0 +1,95 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+import pandas as pd
+
+from Bio import SeqIO
+
+def arguments():
+    parser = ArgumentParser(description="""
+    Takes in a folder of bed files and filters it with matching target.fa files.
+    """)
+
+    parser.add_argument('peaks', help='Folder to exact .bed files.')
+
+    parser.add_argument('targets', help='Folder to exact target.fa files')
+
+    parser.add_argument('-o', '--output', default='results', help='Folder to output to')
+
+    return parser.parse_args()
+
+def n_perc(record):
+    """
+    Returns percent of 'N' of a record
+    """
+    n_count = record.seq.count('N')
+    return 100 * n_count / len(record)
+
+def make_targets_df(targets_file):
+    targets_id, targets_n_perc = [], []
+    for t in SeqIO.parse(targets_file, "fasta"):
+        targets_id.append(t.id)
+        targets_n_perc.append(n_perc(t))
+
+    return pd.DataFrame({'peak_id': targets_id, 'n_perc': targets_n_perc})
+
+def main():
+    args = arguments()
+
+    output_folder = Path(args.output)
+    try:
+        output_folder.mkdir()
+    except OSError:
+        pass
+
+    # Dict for target.fa
+    # Index is gene Name 
+    # f.parts[-2] is parent dir of target.fa. [:-2] gets rid of "*fa" chars at end
+    targets_dict = {f.parts[-2][:-2]: f for f in Path(args.targets).glob("**/target.fa")}
+
+    try:
+        next(Path(args.peaks).glob('*.bed'))
+    except StopIteration:
+        raise OSError('Peaks folder contains no .bed files.')
+
+    if len(targets_dict) == 0:
+        raise OSError('Targets folder contains no target.fa files.')
+
+    for peaks_path in Path(args.peaks).glob("*.bed"):
+        # Get file name drop "_peaks"
+        peaks_name = peaks_path.stem[:-6]
+
+        with peaks_path.open() as peaks_file:
+            peaks = pd.read_table(peaks_file, header=None)
+            peaks.rename(columns={peaks.columns[3]:'peak_id'}, inplace=True)
+
+            with targets_dict[peaks_name].open() as targets_file:
+                targets_df = make_targets_df(targets_file)
+                targets_df_25N = targets_df[targets_df['n_perc'] < 25]
+                targets_df_0N = targets_df[targets_df['n_perc'] == 0]
+
+                peaks_rep = pd.merge(peaks, targets_df, on=['peak_id'])
+
+                peaks_rep_25N = pd.merge(peaks, targets_df_25N, on=['peak_id'])
+                peaks_rep_0N = pd.merge(peaks, targets_df_0N, on=['peak_id'])                 
+
+                with (output_folder / (peaks_name+'.rep.bed')).open('w') as o:
+                    peaks_rep.to_csv(o, sep="\t", index=None, header=None)
+
+                with (output_folder / (peaks_name+'.0Nrep.bed')).open('w') as o:
+                    peaks_rep_0N.to_csv(o, sep="\t", index=None, header=None)
+                with (output_folder / (peaks_name+'.25Nrep.bed')).open('w') as o:
+                    peaks_rep_25N.to_csv(o, sep="\t", index=None, header=None)
+                
+                peaks_nonrep_25N = peaks[~peaks['peak_id'].isin(targets_df_25N['peak_id'])]
+                peaks_nonrep_0N = peaks[~peaks['peak_id'].isin(targets_df_0N['peak_id'])]
+
+                with (output_folder / (peaks_name+'.0Nnonrep.bed')).open('w') as o:
+                    peaks_nonrep_0N.to_csv(o, sep="\t", index=None, header=None)
+                with (output_folder / (peaks_name+'.25Nnonrep.bed')).open('w') as o:
+                    peaks_nonrep_25N.to_csv(o, sep="\t", index=None, header=None)
+
+                
+
+if __name__ == "__main__":
+    main()
